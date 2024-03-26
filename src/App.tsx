459657a7c2e1e5634a5b224f1ce9040a5823c2bd
Pavel Seduan <sphreofdie@gmail.com>
2024-03-26 11:32:28 +0300

    landing dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
 import './App.css'
-import BurgerButton from './components/button.tsx';
-import Title from './components/title.tsx';
-
 function App() {
 
   return (
     <>
-      <BurgerButton
-      />
-      <div className='wrapper'>
-        <Title 
-            text={'Мир, дружба, жвачка!'}
-            level={1}        
-        />
-      </div>  
+     
+      
     </>
   )
 }
